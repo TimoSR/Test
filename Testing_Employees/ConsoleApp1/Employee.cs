05d9e8b813753660452dbbd48c1d5d917ed2eb11
Archit3ct <timothy.s.rasmussen@gmail.com>
2022-09-15 06:26:37 +0200

    Update Project

---
+++ Testing_Employees/ConsoleApp1/Employee.cs
@@ -0,0 +1,128 @@
+using System.Diagnostics;
+
+namespace ConsoleApp1;
+
+public class Employee
+{
+    private int _cpr;
+    
+    private string _firstName;
+    
+    private string _lastName;
+    
+    // I use the enums to have track of what numbers translates to
+
+    private enum Department
+    {
+        HR = 1,
+        Finance = 2,
+        IT = 3,
+        Sales = 4,
+        GeneralServices = 5
+    }
+    
+    private Department _department;
+
+    private int _baseSalary;
+
+    // I use the enums to have track of what numbers translates to
+    
+    public enum EducationalLevel
+    {
+        None = 0,
+        Primary = 1,
+        Secondary = 2,
+        Tertiary = 3
+    }
+
+    private int _educationalLevel;
+
+    private DateOnly _dateOfBirth;
+
+    private DateOnly _dateOfEmployment;
+
+    private string _country;
+
+    public void SetCpr(int cpr)
+    {
+        _cpr = cpr;
+    }
+
+    public int GetCpr()
+    {
+        return _cpr;
+    }
+
+    public void SetFirstName(string firstName)
+    {
+        _firstName = firstName;
+    }
+
+    public string GetFirstName()
+    {
+        return _firstName;
+    }
+
+    public void SetLastName(string lastName)
+    {
+        _lastName = lastName;
+    }
+
+    public string GetLastName()
+    {
+        return _lastName;
+    }
+
+    public void SetDepartment(string department)
+    {
+
+        switch (department)
+        {
+            case "HR":
+                _department = Department.HR; 
+                break;
+            case "Finance":
+                _department = Department.Finance;
+                break;
+            case "IT":
+                _department = Department.IT;
+                break;
+            case "":
+            default:
+
+        }
+        
+
+
+        _department = department;
+    }
+
+    public int GetDepartment()
+    {
+        return _department;
+    }
+
+    public void SetBaseSalary(int baseSalary)
+    {
+        _baseSalary = baseSalary;
+    }
+
+    public int GetBaseSalary()
+    {
+        return _baseSalary;
+    }
+
+    public void SetEducationLevel(int educationalLevel)
+    {
+        _educationalLevel = educationalLevel;
+    }
+
+    public int GetEducationalLevel()
+    {
+        return _educationalLevel;
+    }
+
+
+
+
+}
---
+++ Testing_Employees/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+
+    public enum Department
+    {
+        HR = 1,
+        Finance = 2,
+        IT = 3,
+        Sales = 4,
+        GeneralServices = 5
+    }
+    
+}
